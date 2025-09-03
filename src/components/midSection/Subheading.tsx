f6daafa407a3407ebeff89c819513e9e31861f82
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-09-03 16:54:30 +0200

    added a button for the main section.

--- src/components/midSection/Subheading.tsx
+++ src/components/midSection/Subheading.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Subheading() {
   return (
     <div style={{
-        border: '1px solid red',
         width: '80%',
         height: '10vh',
         display: ' flex',
@@ -14,9 +13,40 @@ export default function Subheading() {
         justifyItems: 'end',
         marginTop: '10%',
         backgroundColor: '#2C263E',
-        color: ' #E9E8EE2'
+        color: ' #FFFFFD'
     }}>
-      <p>nbfsdbfnsdef</p>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+      <p
+      style={{
+        width:'50%',
+        fontSize: '20px',
+      }}>Find out more about how we work</p>
+
+      <div className="viewButton" style={{
+        display:'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}>
+        <button
+          style={{
+        
+            fontSize: "8px",
+            padding: "5px",
+            borderRadius: "0px",
+            backgroundColor: "#2C263F",
+            color: "#bbb9c0ff",
+            border: "1px solid #bbb9c0ff",
+          }}
+        >
+          HOW WE WORK
+        </button>
+      </div>
+      </div>
     </div>
   )
 }
