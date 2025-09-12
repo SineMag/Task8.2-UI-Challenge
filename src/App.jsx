0e512041070e332a462a0e5d3fc321b660eac057
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-09-12 09:18:27 +0200

    reverted back to dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,22 @@
-import React, { useEffect } from 'react'
+import "../src/App.css";
 
-import '../src/App.css'
+import { Navbar } from "./components/Navbar";
 
-import scrollreveal from 'scrollreveal'
+import { Hero } from "./components/Hero";
 
-import { Navbar } from './components/Navbar'
+import { Main } from "./components/Main";
 
-import { Hero } from './components/Hero'
-
-import { Main } from './components/Main'
-
-import { Footer } from './components/Footer'
-
+import { Footer } from "./components/Footer";
 
 export default function App() {
-
-  useEffect(() => {
-
-    const scroll = scrollreveal({
-
-      origin: 'top',
-
-      distance: '80px',
-
-      duration: 2000,
-
-      reset: true,
-
-    })
-
-
-    scroll.reveal(
-
-      `
-
-      #nav,
-
-      #hero,
-
-      #main,
-
-      #footer
-
-      `,
-
-      {
-
-        opacity: 0,
-
-        interval: 300,
-
-      }
-
-    )
-
-  }, [])
-
-
   return (
-
     <>
-
       <Navbar />
 
       <Hero />
 
       <Main />
-<p>bcvbc</p>
       <Footer />
-
     </>
-
-  )
-
+  );
 }
